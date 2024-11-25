9d9f1dbc38a28469bf6045e6aae065d3ab8de900
MAY 06 <MAY 06@LAB2-11>
2024-11-25 09:00:31 +0700

    Add project files.

---
+++ bai18_tinhsothoc.cpp
@@ -0,0 +1,17 @@
+#include  <iostream> <cmath>
+using namespace std;
+int main()
+{
+	cout << "tinh so thoc tren ban co\n:";
+	int soO;
+	double tongthoc = 0;
+	cout << " nhap so o: ";
+	cin >> soO;
+	for (int i = 0; i < soO; i++)
+	{
+		cout << " O thu" << i + 1 << " co " << pow(2, i) << "thoc\n";
+			tongthoc = tongthoc + pow(2, i);
+	}
+	cout << "tong so thoc la: " << tongthoc;
+	return 0;
+}
